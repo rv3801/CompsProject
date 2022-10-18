b36a991d8c76a196e5513443ac8b41c800202308
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2022-10-18 14:42:26 -0700

    Updated models.py
    Updated models with method returning team_name for teams and player_ign for players.

--- ProTracker/models.py
+++ ProTracker/models.py
@@ -5,13 +5,15 @@ class Team(models.Model):
     team_id = models.CharField(max_length = 3)
     team_name = models.CharField(max_length = 50)
     def __str__(self):
-        return "[" + self.team_id + "] " + self.team_name
+        return self.team_name
 
 class Player(models.Model):
     player_ID = models.CharField(max_length = 6)
     player_ign = models.CharField(max_length = 30)
     player_name = models.CharField(max_length = 50)
     team = models.ForeignKey(Team, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.player_ign
 
 class PlayerAccount(models.Model):
     account_id = models.CharField(max_length = 78)

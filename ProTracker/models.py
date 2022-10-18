ff01a5f1f7628b6d75585feeca87e8155bd0485e
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2022-10-17 21:35:36 -0700

    Started DB and Admin
    Started a database with Team, Player, and PlayerAccount models. Enabled admin site.

--- ProTracker/models.py
+++ ProTracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    team_id = models.CharField(max_length = 3)
+    team_name = models.CharField(max_length = 50)
+    def __str__(self):
+        return "[" + self.team_id + "] " + self.team_name
+
+class Player(models.Model):
+    player_ID = models.CharField(max_length = 6)
+    player_ign = models.CharField(max_length = 30)
+    player_name = models.CharField(max_length = 50)
+    team = models.ForeignKey(Team, on_delete = models.CASCADE)
+
+class PlayerAccount(models.Model):
+    account_id = models.CharField(max_length = 78)
+    player = models.ForeignKey(Player, on_delete = models.CASCADE)

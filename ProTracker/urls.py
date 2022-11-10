916dbfbce649e70624d76c861dcd73bb83fa65b1
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2022-11-10 00:25:42 -0800

    Updated urls.py, index.html
    Updated Team url to include /team/ before a team code. Updated index.html to reflect this change

--- ProTracker/urls.py
+++ ProTracker/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:team_code>/', views.team, name = 'team'),
+    path('team/<str:team_code>/', views.team, name = 'team'),
     path('player/<str:player_ign>/', views.player, name = 'player'),
 ]

13e87831a7875c5539b13886b03acd0675ae0ea7
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2022-11-09 20:51:19 -0800

    Updated models.py, views.py, *.html
    Added MatchInfo to Models, edited existing models and model variables. Edited .html templates to reflect changes.

--- ProTracker/views.py
+++ ProTracker/views.py
@@ -5,13 +5,13 @@ from .models import Team, Player
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello, world. You're at the ProTracker index.")
-    team_list = Team.objects.order_by('team_id')
+    team_list = Team.objects.order_by('team_code')
     context = {'team_list': team_list}
     return render(request, 'ProTracker/index.html', context)
 
-def team(request, team_id):
-    team = Team.objects.get(team_id = team_id)
-    player_list = Player.objects.filter(team_id = team)
+def team(request, team_code):
+    team = Team.objects.get(team_code = team_code)
+    player_list = Player.objects.filter(team = team)
     context = {'team': team, 'player_list': player_list}
     return render(request, 'ProTracker/team.html', context)
 

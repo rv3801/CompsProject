4a0ac0dd6e94a20285d738585962d3ecf22bab18
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2022-11-09 20:51:19 -0800

    IP Models
    IN PROGRESS Added MatchInfo to Models, edited existing models.

--- ProTracker/models.py
+++ ProTracker/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Team(models.Model):
-    team_id = models.CharField(max_length = 3)
+    team_code = models.CharField(max_length = 3)
     team_name = models.CharField(max_length = 50)
     def __str__(self):
         return self.team_name
 
 class Player(models.Model):
-    player_ID = models.CharField(max_length = 6)
+    player_id = models.CharField(max_length = 6)
     player_ign = models.CharField(max_length = 30)
     player_name = models.CharField(max_length = 50)
     team = models.ForeignKey(Team, on_delete = models.CASCADE)
@@ -17,4 +17,8 @@ class Player(models.Model):
 
 class PlayerAccount(models.Model):
     account_id = models.CharField(max_length = 78)
+    player = models.ForeignKey(Player, on_delete = models.CASCADE)
+
+class MatchInfo(models.Model):
+    match_id = models.CharField(max_length = 14)
     player = models.ForeignKey(Player, on_delete = models.CASCADE)

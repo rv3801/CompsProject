0bc18288f3193f64dcb6cdb04c67388a6d7aaa44
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2022-11-15 23:07:44 -0800

    Updated RiotWatcher.py
    Renamed variable in get_summoner_level method, removed unnecessary line.

--- ProTracker/RiotWatcher.py
+++ ProTracker/RiotWatcher.py
@@ -1,5 +1,4 @@
 from riotwatcher import LolWatcher
-import os
 
 api_key_file = open("ProTracker\API_Key.txt", "r")
 api_key = api_key_file.read()
@@ -8,5 +7,5 @@ lol_watcher = LolWatcher(api_key)
 my_region = 'na1'
 
 def get_summoner_level(region, summoner_name):
-    me = lol_watcher.summoner.by_name(region, summoner_name)
-    return me["summonerLevel"]
+    return_info = lol_watcher.summoner.by_name(region, summoner_name)
+    return return_info["summonerLevel"]
